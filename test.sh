b34fb7668a2c237456359fcb86c3b71d0d2f04cd
Paul Schroeder <paul.schroeder@nimbix.net>
2017-05-02 18:06:10 -0500

    test

--- test.sh
+++ test.sh
@@ -2,8 +2,11 @@
 
 #sudo service sshd start 2>/dev/null
 #cat /etc/JARVICE/nodes >/data/data/test/$(hostname)
-for i in `cat /etc/JARVICE/nodes`; do
+#for i in `cat /etc/JARVICE/nodes`; do
+for i in `tail -n +2 /etc/JARVICE/nodes`; do
     echo $i
-    ssh -n -f $i "/usr/bin/hostname >/data/data/test/$i"
+    ssh -n -f $i "/bin/hostname >/data/data/test/$i"
 done
+
+sleep 5
 
