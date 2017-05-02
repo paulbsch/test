37093e46055b3ac3d4c7ffc17a7a8e8a366ff5f3
Paul Schroeder <paul.schroeder@nimbix.net>
2017-05-02 17:48:44 -0500

    test

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 #sudo service sshd start 2>/dev/null
 #cat /etc/JARVICE/nodes >/data/data/test/$(hostname)
 for i in `cat /etc/JARVICE/nodes`; do
-    ssh -n -f $i 'touch /data/data/test/$(hostname)'
+    ssh -n -f $i "hostname >/data/data/test/$i"
 done
 
