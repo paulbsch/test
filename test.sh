1348ebdc7592234bb37ef59c7942040895389b22
Paul Schroeder <paul.schroeder@nimbix.net>
2017-05-02 22:51:58 -0500

    test

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@
 #cat /etc/JARVICE/nodes >/data/data/test/$(hostname)
 #for i in `cat /etc/JARVICE/nodes`; do
 cat /etc/JARVICE/nodes
+echo "*******"
 for i in `tail -n +2 /etc/JARVICE/nodes`; do
     echo $i
     #ssh -n -f $i "/bin/hostname >/data/data/test/$i"
